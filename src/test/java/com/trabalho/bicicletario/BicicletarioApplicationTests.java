72e536766656d66ce6250da9cafab71144733993
Thaissa Lopes <117691038+thatalps@users.noreply.github.com>
2024-12-07 14:43:29 -0300

    sc commit

---
+++ src/test/java/com/trabalho/bicicletario/BicicletarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trabalho.bicicletario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BicicletarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
